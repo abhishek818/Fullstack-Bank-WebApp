fb30ac059b6d7d5907b40f28ad985bbcfb497b84
Abhishek Kumar Gupta <abhishekguptaatweb17@gmail.com>
2021-03-13 13:02:41 +0530

    add all routes

--- client/src/components/ListBanks.js
+++ client/src/components/ListBanks.js
@@ -21,11 +21,20 @@ const ListBanks = () => {
     getBanks();
   });
 
+  // const submitBankHandler = (e) =>
+  // {
+  //   e.preventDefault();
+  // }
 
   return (
     <Fragment>
+      <h3>The Bank Database was taken from: &nbsp;  
+        <a href="https://github.com/snarayanank2/indian_banks" target="_blank" rel="noreferrer">
+          Click Here!
+        </a>
+      </h3>
       <span>
-      <select className="select mt-5"
+      <select className="select mt-3"
        value={query} onChange = {(e) =>
       {
         setQuery(e.target.value);
@@ -38,7 +47,7 @@ const ListBanks = () => {
         <option value="Ahmedabad"> Ahmedabad </option>
       </select>
       
-      <input type="text" className="search" placeholder="Search" 
+      <input type="text" className="search" placeholder="Search any column" 
         value={query} onChange = {(e) =>
       {
         setQuery(e.target.value);
@@ -47,10 +56,24 @@ const ListBanks = () => {
     
     </span>
     
-    <h4 className="header"> Search Results for "<span style={{ color:"red" }}>{query}</span>" </h4>
-      <table style={{ maxWidth: "90%" }} className="table table-bordered table-responsive-xl
+    <h4 className="header"> Search Results for "<span style={{ color:"pink", font:"10px" }}>{query}</span>" </h4>
+    
+    <form action="/api/branches/insert" method="post" className="form">
+      <input size="10" type="text" name="ifsc" placeholder="Enter Ifsc" required />
+      <input size="8" type="text" name="id" placeholder="Enter Bank Id" required />
+      <input type="text" name="branch" className="todo-input" placeholder="Enter Branch" required />
+      <input size="30" name="address" type="text" className="todo-input" placeholder="Enter Address" required />
+      <input type="text" name="city" className="todo-input" placeholder="Enter City" required />
+      <input type="text" name="district" className="todo-input" placeholder="Enter District" required />
+      <input type="text" name="state" className="todo-input" placeholder="Enter State" required />
+      
+      <button type="submit" className="btn btn-info">Add a Record !
+      </button>
+    </form>
+    
+    <table style={{ maxWidth: "95%" }} className="table table-bordered table-responsive-xl
        mb-5 mt-4 table-hover
-       table-striped text-center
+       text-center
        center">
         <thead className="thead-dark">
           <tr>
@@ -61,6 +84,7 @@ const ListBanks = () => {
             <th>City</th>
             <th>District</th>
             <th>State</th>
+            <th>Delete row</th>
           </tr>
         </thead>
         <tbody>
@@ -73,6 +97,13 @@ const ListBanks = () => {
               <td>{bank.city}</td>
               <td>{bank.district}</td>
               <td>{bank.state}</td>
+              <td>
+                <form action="/api/branches/delete" method="post">
+                  <button name="ifsc" value={bank.ifsc} id={bank.ifsc} style={{ backgroundColor: "#d9534f" }} 
+                    type="submit" className="btn btn-danger">Delete
+                  </button>
+                </form>
+              </td>
             </tr>
           ))}
         </tbody>
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
 
 const path = require('path');
 const pool = require("./db");
+var bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
-console.log("PORT= "+process.env.PORT);
 
-// app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // console.log(path.join(__dirname, "client/build"));
 if(process.env.NODE_ENV === "production")
@@ -81,26 +80,6 @@ app.get("/api/branches/autocomplete", async (req, res) =>
       order by ifsc 
       LIMIT ${limit} 
       OFFSET ${offset}`);
-    
-    // let filteredData = [];
-    // filteredData.push("ifsc ,bank_id , branch , address , city , district , state");
-
-    // for(var i=0;i<results[1].rows.length;i++)
-    //   {
-    //      let eachFilteredData = [];
-    //      var data = results[1].rows[i];
-    //      eachFilteredData.push(data.ifsc);
-    //      eachFilteredData.push(data.bank_id);
-    //      eachFilteredData.push(data.branch);
-    //      eachFilteredData.push(data.address);
-    //      eachFilteredData.push(data.city);
-    //      eachFilteredData.push(data.district);
-    //      eachFilteredData.push(data.state);
-         
-    //      filteredData.push(eachFilteredData);
-    //   }
-     
-    // res.send(JSON.stringify(filteredData, null, 2));
       
     res.send(results[1].rows);
   } 
@@ -110,29 +89,57 @@ app.get("/api/branches/autocomplete", async (req, res) =>
     }
 });
 
-// app.get("/api/branches/autocomplete", async (req, res) =>
-// {
-//   try 
-//   {
-//     var branch = req.query.q;
-//     var limit = req.query.limit;
-//     var offset = req.query.offset;
+app.post("/api/branches/insert", async (req, res) => {
+  
+  try 
+  {
+    const { ifsc, id, branch, address, city, district, state } = req.body;
+    
+    var iD=parseInt(id);
+    // console.log(typeof(iD));
 
-//     const results = await pool.query(`SET client_encoding to 'win1252';  
-//       SELECT ifsc, bank_id, branch, address, city, district, state FROM branches
-//       where branch like '${branch}%'
-//       order by ifsc 
-//       LIMIT ${limit} 
-//       OFFSET ${offset}`);
+    const insert = await pool.query(`INSERT INTO branches VALUES (
+      '${ifsc}', ${iD}, 
+      '${branch}', '${address}', 
+      '${city}', '${district}', '${state}'
+    )`);
+
+    res.send("<h2>Successfully Inserted into the database: <br/><br/>" + 
+    "Ifsc: " + ifsc + "<br/>" +
+    "Bank_id: " + id + "<br/>" +
+    "Branch: " + branch + "<br/>" +
+    "Address: " + address + "<br/>" +
+    "City: " + city + "<br/>" +
+    "District: " + district + "<br/>" +
+    "State: " + state + "</h2></br></br>" +
+    "<br/><button><a href='/' alt='Go Back'>Go Back!</a></button>"
+    );
+
+  } 
+    catch (error) {
+      res.send("<br/><h2>" + error.message + "</h2>"
+      +"<br/><button><a href='/' alt='Go Back'>Go Back!</a></button>");
+    }
+});
+
+app.post("/api/branches/delete", async (req, res) => {
+  
+  try 
+  {
+    console.log(req.body.ifsc);
+  
+    const del = await pool.query(`Delete from branches where ifsc = '${req.body.ifsc}' `);
     
-    
-//     res.send(results[1].rows);
-//   } 
-//     catch (err) 
-//     {
-//       console.error(err.message);
-//     }
-// });
+    res.send("Row with id= " + req.body.ifsc + " deleted successfully" + 
+    " <br/><br/><button><a href='/' alt='Go Back'>Go Back!</a></button>");
+
+  } 
+    catch (error) {
+      res.send("<br/><h2>" + error.message + "</h2>"
+      +"<br/><button><a href='/' alt='Go Back'>Go Back!</a></button>");
+    }
+});
+
 
 
 app.listen(PORT, () => 
